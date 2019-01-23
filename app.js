df1c4974afc0dd7b6fcfd2da8c03985e24f82f7b
steven <steven@lalalab.com>
2019-01-23 16:27:21 +0100

    testing /deeplink to lalalab

--- app.js
+++ app.js
@@ -5,31 +5,31 @@ const deeplink = require('node-deeplink')
 const { router } = require('./routes/routes.js')
 
 const app = express()
-let url = ''
-
-var middleware = (req, res, next) => {
-  url = req.url
-  next(url)
-}
+let path = ''
+let account = ''
 
 app.use('/', (req, res, next) => {
+  const tmp = req.url
+  path = tmp.split('?')[0]
+  account = tmp.split('?')[1]
   next()
-})
-app.use(middleware)
-
-app.get('/favicon.ico', (req, res) => {
-  res.status(200).send('On sen fous')
 })
 
 app.get('/stupid', (req, res) => {
-  res.status(200).send('stupid rooutes')
+  res.status(200).send(`value of account from stupid ${account}`)
 })
 
-app.get('/*', deeplink({
-  fallback: 'https://wwww.lalalab.com' + url,
-  android_package_name: 'com.invaderscorp.polagram',
-  ios_store_link: 'https://itunes.apple.com/us/app/lalalab-photo-printing/id586420569?mt=8' + '?url=' + encodeUrl('https://app.lalalab.com' + url)
+app.get('/deeplink', deeplink({
+  fallback: 'https://www.lalalab.com/fr'
+//   android_package_name: 'com.invaderscorp.polagram',
+//   ios_store_link: 'https://itunes.apple.com/us/app/lalalab-photo-printing/id586420569?mt=8' + '?url=' + encodeUrl('https://app.lalalab.com' + url)
 }))
+
+// app.get('*', deeplink({
+//   fallback: 'https://wwww.lalalab.com' + url,
+//   android_package_name: 'com.invaderscorp.polagram',
+//   ios_store_link: 'https://itunes.apple.com/us/app/lalalab-photo-printing/id586420569?mt=8' + '?url=' + encodeUrl('https://app.lalalab.com' + url)
+// }))
 
 module.exports = {
   app,
