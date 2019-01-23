f651489123ba4dc16ceba8cc29b2d41d0e953ded
steven <steven@lalalab.com>
2019-01-23 14:44:47 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const encodeUrl = require('encodeurl')
+const deeplink = require('node-deeplink')
+
+const { router } = require('./routes/routes.js')
+
+const app = express()
+let url = ''
+
+var middleware = (req, res, next) => {
+  url = req.url
+  next(url)
+}
+
+app.use(middleware)
+
+app.get('/*', deeplink({
+  fallback: 'https://wwww.lalalab.com' + url,
+  android_package_name: 'com.invaderscorp.polagram',
+  ios_store_link: 'https://itunes.apple.com/us/app/lalalab-photo-printing/id586420569?mt=8' + '?url=' + encodeUrl('https://app.lalalab.com' + url)
+}))
+
+module.exports = {
+  app,
+}
---
+++ routes/routes.js
@@ -0,0 +1,11 @@
+const app = require('express')
+const router = app.Router()
+
+// router.get('/', (req, res) => {
+//   console.log(req.device)
+//   res.status(200).send(`${is.firefox()}`)
+// })
+
+module.exports = {
+  router
+}
